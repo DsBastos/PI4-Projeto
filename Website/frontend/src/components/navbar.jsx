277ab116d7c727fee8d582a2867c07b488e96d0f
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-11-14 20:33:08 +0000

    correções da navbar

--- Website/frontend/src/components/navbar.jsx
+++ Website/frontend/src/components/navbar.jsx
@@ -3,36 +3,141 @@ import logo from "../assets/mygreenpointlogo.png";
 import { Link } from "react-dom";
 
 export function Navbar() {
-    return (
-        <div>
-            <nav class="navbar fixed-top navbar-expand-lg" style={{ height: '7em' }}>
-                <div class="container">
-                    <a className="navbar-brand"><img src={logo} alt="My Green Point" style={{ maxWidth: '180px', margin: '18px', height: 'auto' }} /></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" ar>
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                        <ul class="navbar-nav align-items-end ">
-                            <li class="nav-item" style={{ width: '137px', height: '40px', background: '#6CCA98', borderRadius: '99px', marginRight: '10px', marginTop: '10px'  }}>
-                                <a class="nav-link" type='button' style={{ color: 'white' }} Link="#">Inicio</a>
-                            </li>
-                            <li class="nav-item" style={{ width: '137px', height: '40px', background: '#6CCA98', borderRadius: '99px', marginRight: '10px', marginTop: '10px' }}>
-                                <a class="nav-link" type='button' style={{ color: 'white' }} Link="#">Descarregar</a>
-                            </li>
-                            <li class="nav-item" style={{ width: '137px', height: '40px', background: '#6CCA98', borderRadius: '99px', marginRight: '10px', marginTop: '10px' }}>
-                                <a class="nav-link" type='button' style={{ color: 'white' }} Link="#">Objetivo</a>
-                            </li>
-                            <li class="nav-item" style={{ width: '137px', height: '40px', background: '#6CCA98', borderRadius: '99px', marginRight: '10px', marginTop: '10px' }}>
-                                <a class="nav-link " type='button' style={{ color: 'white' }} Link="#">Oferecemos</a>
-                            </li>
-                            <li class="nav-item" style={{ width: '137px', height: '40px', background: '#6CCA98', borderRadius: '99px', marginRight: '10px', marginTop: '10px' }}>
-                                <a class="nav-link " type='button' style={{ color: 'white' }} Link="#"> Atualizações</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <nav
+      class="navbar fixed-top navbar-expand-lg text-center"
+      style={{ height: "7em" }}
+    >
+      <div class="container">
+        <a className="navbar-brand">
+          <img
+            src={logo}
+            alt="My Green Point"
+            style={{ maxWidth: "180px", margin: "18px", height: "auto" }}
+          />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          ar
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav align-items-end ">
+            <li
+              class="nav-item"
+              style={{
+                width: "137px",
+                height: "40px",
+                background: "#6CCA98",
+                borderRadius: "99px",
+                marginRight: "10px",
+                marginTop: "10px",
+              }}
+            >
+              <a
+                class="nav-link"
+                type="button"
+                style={{ color: "white" }}
+                Link="#"
+              >
+                Inicio
+              </a>
+            </li>
+            <li
+              class="nav-item"
+              style={{
+                width: "137px",
+                height: "40px",
+                background: "#6CCA98",
+                borderRadius: "99px",
+                marginRight: "10px",
+                marginTop: "10px",
+              }}
+            >
+              <a
+                class="nav-link"
+                type="button"
+                style={{ color: "white" }}
+                Link="#"
+              >
+                Descarregar
+              </a>
+            </li>
+            <li
+              class="nav-item"
+              style={{
+                width: "137px",
+                height: "40px",
+                background: "#6CCA98",
+                borderRadius: "99px",
+                marginRight: "10px",
+                marginTop: "10px",
+              }}
+            >
+              <a
+                class="nav-link"
+                type="button"
+                style={{ color: "white" }}
+                Link="#"
+              >
+                Objetivo
+              </a>
+            </li>
+            <li
+              class="nav-item"
+              style={{
+                width: "137px",
+                height: "40px",
+                background: "#6CCA98",
+                borderRadius: "99px",
+                marginRight: "10px",
+                marginTop: "10px",
+              }}
+            >
+              <a
+                class="nav-link "
+                type="button"
+                style={{ color: "white" }}
+                Link="#"
+              >
+                Oferecemos
+              </a>
+            </li>
+            <li
+              class="nav-item"
+              style={{
+                width: "137px",
+                height: "40px",
+                background: "#6CCA98",
+                borderRadius: "99px",
+                marginRight: "10px",
+                marginTop: "10px",
+              }}
+            >
+              <a
+                class="nav-link "
+                type="button"
+                style={{ color: "white" }}
+                Link="#"
+              >
+                {" "}
+                Atualizações
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+      </div>
+    </nav>
+  );
 }
 export default Navbar;
