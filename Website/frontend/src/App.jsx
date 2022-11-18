2c10e3a01b56c54030e1226dd3a23d7ba323bf79
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-11-18 19:40:32 +0000

    ajustes de class de uma imagem

--- Website/frontend/src/App.jsx
+++ Website/frontend/src/App.jsx
@@ -48,7 +48,7 @@ export function App() {
       <section className="my-5">
         <div className="container">
           <div className="row py-5">
-            <div className="col d-none d-lg-block d-flex justify-content-center">
+            <div className="col d-flex justify-content-center">
               <img
                 src={imageDescarregarApp}
                 class="img-fluid"
