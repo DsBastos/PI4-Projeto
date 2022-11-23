6fc6da9df2dae46a6ae1f4d639b420cba58498bd
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-11-23 23:34:11 +0000

    dashboard

---
+++ Backoffice/src/pages/dashboardIndex.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Topnav } from "./components/Topnav";
+
+function dashboardIndex() {
+  return (
+    <div className="app">
+      <header>
+        <Topnav role="administrador" nome="ROBERTO" />
+      </header>
+      <main className="">sdfsdfsddf</main>
+    </div>
+  );
+}
+
+export default dashboardIndex;
