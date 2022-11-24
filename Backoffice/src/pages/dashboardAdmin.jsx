9014a7276c9f9d6f483052fd5b79d9835e7473a5
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-11-24 16:29:20 +0000

    mudanças da propriedade class para className

--- Backoffice/src/pages/dashboardAdmin.jsx
+++ Backoffice/src/pages/dashboardAdmin.jsx
@@ -13,115 +13,115 @@ function dashboardAdmin() {
       </header>
       <main className="container-fluid">
         <h2>Dashboard</h2>
-        <div class="d-flex mt-5">
+        <div className="d-flex mt-5">
           <m.div
-            class="card"
+            className="card"
             whileHover={{
               scale: 1.05,
               boxShadow: "0 .5rem 1rem rgba(0,0,0,.15) ",
             }}
             style={{ maxWidth: "18rem", height: "12rem" }}
           >
-            <div class="card-body">
+            <div className="card-body">
               <div className="d-flex justify-content-between">
-                <h5 class="card-title text-muted pe-4 fw-bold">
+                <h5 className="card-title text-muted pe-4 fw-bold">
                   Nº de Responsável da região turística
                 </h5>
                 <img src={icongroup} alt="" />
               </div>
-              <p class="card-text h1 mt-3">25</p>
+              <p className="card-text h1 mt-3">25</p>
             </div>
           </m.div>
           <m.div
-            class="card ms-4"
+            className="card ms-4"
             whileHover={{
               scale: 1.05,
               boxShadow: "0 .5rem 1rem rgba(0,0,0,.15) ",
             }}
             style={{ maxWidth: "18rem", height: "12rem" }}
           >
-            <div class="card-body">
+            <div className="card-body">
               <div className="d-flex justify-content-between">
-                <h5 class="card-title text-muted pe-4 fw-bold">
+                <h5 className="card-title text-muted pe-4 fw-bold">
                   Nº de Responsável da região turística
                 </h5>
                 <img src={icongroup} alt="" />
               </div>
-              <p class="card-text h1 mt-3">25</p>
+              <p className="card-text h1 mt-3">25</p>
             </div>
           </m.div>
           <m.div
-            class="card ms-4"
+            className="card ms-4"
             whileHover={{
               scale: 1.05,
               boxShadow: "0 .5rem 1rem rgba(0,0,0,.15) ",
             }}
             style={{ maxWidth: "18rem", height: "12rem" }}
           >
-            <div class="card-body">
+            <div className="card-body">
               <div className="d-flex justify-content-between">
-                <h5 class="card-title text-muted pe-4 fw-bold">
+                <h5 className="card-title text-muted pe-4 fw-bold">
                   Nº de Responsável da região turística
                 </h5>
                 <img src={icongroup} alt="" />
               </div>
-              <p class="card-text h1 mt-3">25</p>
+              <p className="card-text h1 mt-3">25</p>
             </div>
           </m.div>
         </div>
         <div className="d-flex mt-4">
           <m.div
-            class="card"
+            className="card"
             whileHover={{
               scale: 1.05,
               boxShadow: "0 .5rem 1rem rgba(0,0,0,.15) ",
             }}
             style={{ maxWidth: "18rem", height: "12rem" }}
           >
-            <div class="card-body">
+            <div className="card-body">
               <div className="d-flex justify-content-between">
-                <h5 class="card-title text-muted pe-4 fw-bold">
+                <h5 className="card-title text-muted pe-4 fw-bold">
                   Nº de Responsável da região turística
                 </h5>
                 <img src={icongroup} alt="" />
               </div>
-              <p class="card-text h1 mt-3">25</p>
+              <p className="card-text h1 mt-3">25</p>
             </div>
           </m.div>
           <m.div
-            class="card ms-4"
+            className="card ms-4"
             whileHover={{
               scale: 1.05,
               boxShadow: "0 .5rem 1rem rgba(0,0,0,.15) ",
             }}
             style={{ maxWidth: "18rem", height: "12rem" }}
           >
-            <div class="card-body">
+            <div className="card-body">
               <div className="d-flex justify-content-between">
-                <h5 class="card-title text-muted pe-4 fw-bold">
+                <h5 className="card-title text-muted pe-4 fw-bold">
                   Nº de Responsável da região turística
                 </h5>
                 <img src={icongroup} alt="" />
               </div>
-              <p class="card-text h1 mt-3">25</p>
+              <p className="card-text h1 mt-3">25</p>
             </div>
           </m.div>
           <m.div
-            class="card ms-4"
+            className="card ms-4"
             whileHover={{
               scale: 1.05,
               boxShadow: "0 .5rem 1rem rgba(0,0,0,.15) ",
             }}
             style={{ maxWidth: "18rem", height: "12rem" }}
           >
-            <div class="card-body">
+            <div className="card-body">
               <div className="d-flex justify-content-between">
-                <h5 class="card-title text-muted pe-4 fw-bold">
+                <h5 className="card-title text-muted pe-4 fw-bold">
                   Nº de Responsável da região turística
                 </h5>
                 <img src={icongroup} alt="" />
               </div>
-              <p class="card-text h1 mt-3">25</p>
+              <p className="card-text h1 mt-3">25</p>
             </div>
           </m.div>
         </div>
