bc42cca855fa0d6c2876d623cb16f1c6108c78bb
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-11-24 01:48:00 +0000

    cards feitos

--- Backoffice/src/pages/dashboardIndex.jsx
+++ Backoffice/src/pages/dashboardIndex.jsx
@@ -1,13 +1,87 @@
 import React from "react";
 import { Topnav } from "../components/Topnav";
 
+import icongroup from "../assets/icongroup.svg";
 function dashboardIndex() {
   return (
     <>
+      {/* Colocar aqui o componente da sidebar */}
       <header>
         <Topnav role="administrador" nome="ROBERTO" />
       </header>
-      <main className="">sdfsdfsddf</main>
+      <main className="container-fluid">
+        <h2>Dashboard</h2>
+        <div class="d-flex mt-5">
+          <div class="card" style={{ maxWidth: "18rem", height: "12rem" }}>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 class="card-title text-muted pe-4 fw-bold">
+                  Nº de Responsável da região turística
+                </h5>
+                <img src={icongroup} alt="" />
+              </div>
+              <p class="card-text h1 mt-3">25</p>
+            </div>
+          </div>
+          <div class="card ms-4" style={{ maxWidth: "18rem", height: "12rem" }}>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 class="card-title text-muted pe-4 fw-bold">
+                  Nº de Responsável da região turística
+                </h5>
+                <img src={icongroup} alt="" />
+              </div>
+              <p class="card-text h1 mt-3">25</p>
+            </div>
+          </div>
+          <div class="card ms-4" style={{ maxWidth: "18rem", height: "12rem" }}>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 class="card-title text-muted pe-4 fw-bold">
+                  Nº de Responsável da região turística
+                </h5>
+                <img src={icongroup} alt="" />
+              </div>
+              <p class="card-text h1 mt-3">25</p>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex mt-4">
+          <div class="card" style={{ maxWidth: "18rem", height: "12rem" }}>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 class="card-title text-muted pe-4 fw-bold">
+                  Nº de Responsável da região turística
+                </h5>
+                <img src={icongroup} alt="" />
+              </div>
+              <p class="card-text h1 mt-3">25</p>
+            </div>
+          </div>
+          <div class="card ms-4" style={{ maxWidth: "18rem", height: "12rem" }}>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 class="card-title text-muted pe-4 fw-bold">
+                  Nº de Responsável da região turística
+                </h5>
+                <img src={icongroup} alt="" />
+              </div>
+              <p class="card-text h1 mt-3">25</p>
+            </div>
+          </div>
+          <div class="card ms-4" style={{ maxWidth: "18rem", height: "12rem" }}>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 class="card-title text-muted pe-4 fw-bold">
+                  Nº de Responsável da região turística
+                </h5>
+                <img src={icongroup} alt="" />
+              </div>
+              <p class="card-text h1 mt-3">25</p>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
