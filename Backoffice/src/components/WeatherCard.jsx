44551d45def677ac4175ba903796e7a957144433
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-12-02 14:02:52 +0000

    test com a api do tempo

--- Backoffice/src/components/WeatherCard.jsx
+++ Backoffice/src/components/WeatherCard.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { usePosition } from "../hooks/usePosition";
+
 import axios from "axios";
 
 export function WeatherCard() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState([]);
+  const watch = true;
+  const { latitude, longitude } = usePosition(watch);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=&appid=d02fd25e2c26e6e422160e087f7388d0`;
-
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d02fd25e2c26e6e422160e087f7388d0`;
   axios.get(url).then((response) => {
     setData(response.data);
     console.log(response.data);
   });
-
   return (
     <div className="location">
-      <p>{data.name}</p>
+      <h1>{data.name}</h1>
     </div>
   );
 }
