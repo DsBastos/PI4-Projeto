feebed0a899f2cfa4d3fb345c1c06459166f37bc
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-12-02 12:11:07 +0000

    weather card com as localizacao atual

--- Backoffice/src/components/WeatherCard.jsx
+++ Backoffice/src/components/WeatherCard.jsx
@@ -3,16 +3,14 @@ import axios from "axios";
 
 export function WeatherCard() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState([]);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=viseu&appid=d02fd25e2c26e6e422160e087f7388d0`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=&appid=d02fd25e2c26e6e422160e087f7388d0`;
 
-  const searchLocation = (event) => {
-    axios.get(url).then((response) => {
-      setData(response.data);
-      console.log(response.data);
-    });
-  };
+  axios.get(url).then((response) => {
+    setData(response.data);
+    console.log(response.data);
+  });
 
   return (
     <div className="location">
