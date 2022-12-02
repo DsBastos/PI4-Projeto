1709220c7104f8b99117a8dc4b0d4eaf292301de
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-12-02 20:13:20 +0000

    codigo comentado da api de tempo

--- Backoffice/src/components/WeatherCard.jsx
+++ Backoffice/src/components/WeatherCard.jsx
@@ -7,25 +7,25 @@ export function WeatherCard() {
   const [data, setData] = useState({});
   const watch = true;
   const { latitude, longitude } = usePosition(watch);
-  useEffect(() => {
+  /* useEffect(() => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d02fd25e2c26e6e422160e087f7388d0`;
     axios.get(url, { timeout: 0 }).then((response) => {
       setData(response.data);
       console.log(response.data);
     });
-  });
+  }); */
   return (
-  <div className="cardimage img-fluid">
-    <div className="p-5">
-    <h1 className="card-text text-end">Viseu</h1>
-    <h3 className="card-text text-end">Neve</h3>
-    <h1 className="card-text text-end">-4º</h1>
+    <div className="cardimage img-fluid">
+      <div className="p-5">
+        <h1 className="card-text text-end">Viseu</h1>
+        <h3 className="card-text text-end">Neve</h3>
+        <h1 className="card-text text-end">-4º</h1>
+      </div>
+      <div className="p-5">
+        <h2 className="card-title">08:30H</h2>
+        <h5 className="card-text">Quarta-feira,18 de novembro 2022</h5>
+      </div>
     </div>
-    <div className="p-5">
-    <h2 className="card-title">08:30H</h2>
-    <h5 className="card-text">Quarta-feira,18 de novembro 2022</h5>
-    </div>
-  </div>
   );
 }
 
