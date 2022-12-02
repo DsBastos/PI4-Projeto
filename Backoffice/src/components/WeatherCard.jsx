1ca03a03f3a038b4113c0c105161b88587acb24b
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-12-02 20:11:10 +0000

    teste com a apis de tempo

--- Backoffice/src/components/WeatherCard.jsx
+++ Backoffice/src/components/WeatherCard.jsx
@@ -7,11 +7,12 @@ export function WeatherCard() {
   const [data, setData] = useState({});
   const watch = true;
   const { latitude, longitude } = usePosition(watch);
-
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d02fd25e2c26e6e422160e087f7388d0`;
-  axios.get(url).then((response) => {
-    setData(response.data);
-    console.log(response.data);
+  useEffect(() => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d02fd25e2c26e6e422160e087f7388d0`;
+    axios.get(url, { timeout: 0 }).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
   });
   return (
     <div className="location">
