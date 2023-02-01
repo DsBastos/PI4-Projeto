63d8db7d8a238b0e5ea21fe0c36029e9f9f95d3b
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2023-02-01 17:39:31 +0000

    buscar nome do utilizador

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,35 +1,36 @@
-require("dotenv").config();
-const express = require("express");
-var cors = require("cors");
-const app = express();
-const path = require("path");
-require("./models/associations");
-const createError = require("http-errors");
-const errorHandler = require("./middleware/errorHandler");
-const verifyJWT = require("./middleware/verifyJWT");
-const cookieParser = require("cookie-parser");
-const corsOptions = require("./config/corsOptions");
-const credentials = require("./middleware/credentials");
+require('dotenv').config()
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const path = require('path')
+require('./models/associations')
+const createError = require('http-errors')
+const errorHandler = require('./middleware/errorHandler')
+const verifyJWT = require('./middleware/verifyJWT')
+const cookieParser = require('cookie-parser')
+const corsOptions = require('./config/corsOptions')
+const credentials = require('./middleware/credentials')
 
 // Handle options credentials check - before CORS!
 // and fetch cookies credentials requirement
 
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({ credentials: true, origin: true }))
 
-app.set("port", process.env.PORT || 3333);
+app.set('port', process.env.PORT || 3333)
 //Middlewares
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-app.use(cookieParser());
+app.use(cookieParser())
 
 //serve static files
-app.use("/", express.static(path.join(__dirname, "../public")));
+app.use('/', express.static(path.join(__dirname, '../public')))
 
 //Routes
 
-const autenticacaoRoute = require("./routes/authRoute.js");
-const refreshRoute = require("./routes/refreshRoute.js");
+const autenticacaoRoute = require('./routes/authRoute.js')
+const refreshRoute = require('./routes/refreshRoute.js')
+const logoutRoute = require('./routes/logoutRoute.js')
 
 const clienteRoute = require('./routes/clienteRoute.js')
 const pontoTuristicoRoute = require('./routes/pontoTuristicoRoute.js')
@@ -40,37 +41,39 @@ const tipoutilizadoresRoute = require('./routes/tipoUtilizadorRoute.js')
 const tipologiaRoute = require('./routes/tipologiaRoute.js')
 const visitaRoute = require('./routes/visitaRoute.js')
 const voucherRoute = require('./routes/voucherRoute.js')
-const websiteRoute = require('./routes/websiteRoute.js');
-const utilizadoresRoute = require('./routes/utilizadoresRoute.js');
+const websiteRoute = require('./routes/websiteRoute.js')
+const utilizadoresRoute = require('./routes/utilizadoresRoute.js')
 
-app.use("/auth", autenticacaoRoute);
-app.use("/refresh", refreshRoute);
-app.use(verifyJWT);
-app.use('/utilizadores',utilizadoresRoute)
-app.use('/tipoutilizadores',tipoutilizadoresRoute)
+app.use('/auth', autenticacaoRoute)
+app.use('/refresh', refreshRoute)
+app.use('/logout', logoutRoute)
+
+app.use(verifyJWT)
+app.use('/utilizadores', utilizadoresRoute)
+app.use('/tipoutilizadores', tipoutilizadoresRoute)
 app.use('/cliente', clienteRoute)
 app.use('/pontoturistico', pontoTuristicoRoute)
 app.use('/recompensa', recompensaRoute)
 app.use('/regiaoturistica', regiaoTuristicaRoute)
 app.use('/reserva', reservaRoute)
-app.use('/tipologia', tipologiaRoute);
+app.use('/tipologia', tipologiaRoute)
 app.use('/visita', visitaRoute)
 app.use('/voucher', voucherRoute)
-app.use('/website', websiteRoute);
+app.use('/website', websiteRoute)
 
-app.all("*", (req, res, next) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ error: "404 Not found" });
+app.all('*', (req, res, next) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not found' })
   } else {
-    res.type("txt").send("404 Not found");
+    res.type('txt').send('404 Not found')
   }
-});
+})
 
-app.use(createError);
+app.use(createError)
 
-app.listen(app.get("port"), () => {
-  console.log("Start server on port " + app.get("port"));
-});
+app.listen(app.get('port'), () => {
+  console.log('Start server on port ' + app.get('port'))
+})
--- Backend/src/controllers/refreshTokenController.js
+++ Backend/src/controllers/refreshTokenController.js
@@ -1,25 +1,25 @@
-var sequelize = require("../models/database");
-const Sequelize = require("sequelize");
-var utilizador = require("../models/utilizadoresModel");
+var sequelize = require('../models/database')
+const Sequelize = require('sequelize')
+var utilizador = require('../models/utilizadoresModel')
 
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const handleRefreshToken = async (req, res) => {
-  const cookies = req.cookies;
+  const cookies = req.cookies
 
-  if (!cookies?.jwt) return res.sendStatus(401); //unauthorized (no token found)
+  if (!cookies?.jwt) return res.sendStatus(401) //unauthorized (no token found)
 
-  console.log(cookies.jwt);
-  const refreshToken = cookies.jwt;
+  console.log(cookies.jwt)
+  const refreshToken = cookies.jwt
 
-  const foundUser = await utilizador.findOne({ refreshToken });
-  if (!foundUser) return res.status(403); //forbidden
+  const foundUser = await utilizador.findOne({ where: { refreshToken } })
+  if (!foundUser) return res.status(403) //forbidden
 
   // evaluate JWTs
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-    if (err || foundUser.u_email !== decoded.u_email) return res.status(403);
-    const role = Object.values(foundUser.tu_id);
+    if (err || foundUser.u_email !== decoded.u_email) return res.status(403)
+    const role = Object.values(foundUser.tu_id)
     const accessToken = jwt.sign(
       {
         UserInfo: {
@@ -28,10 +28,10 @@ const handleRefreshToken = async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "15m" }
-    );
-    res.json({role, accessToken });
-  });
-};
+      { expiresIn: '15m' }
+    )
+    res.json({ role, accessToken })
+  })
+}
 
-module.exports = { handleRefreshToken };
+module.exports = { handleRefreshToken }
