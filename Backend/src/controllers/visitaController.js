8afe65afde60d5bde499ce490581e8b83e88857e
Constança Fernandes <constancafernandes22@gmail.com>
2023-02-01 15:14:19 +0000

    API
    Melhorias para corresponder com as necessidades da aplicação móvel

--- Backend/src/controllers/visitaController.js
+++ Backend/src/controllers/visitaController.js
@@ -1,4 +1,4 @@
-var visita = require('../models/VisitaModel');
+var visita = require('../models/visitaModel');
 
 const controllers = {};
 var sequelize = require("../models/database");
@@ -6,83 +6,103 @@ const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 const createError = require('http-errors');
 const pontoTuristicos = require('../models/pontoTuristicoModel');
+const { fn } = require("sequelize");
 
 controllers.getAllVisita = async (req, res, next) => {
-    try {
-        const data = await visita.findAll({
-            include:[{model:pontoTuristicos, attributes:['pt_nome']}]
+  try {
+    const data = await visita.findAll({
+      include: [{ model: pontoTuristicos, attributes: ['pt_nome', 'pt_regiao'] }]
 
-        });
-        res.send({ success: true, data: data });
-    } catch (error) {
-        next(error)
-    }
+    });
+    res.send({ success: true, data: data });
+  } catch (error) {
+    next(error)
+  }
+};
+
+controllers.getNextVisitaByPtId = async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const currentDate = fn("NOW");
+    const data = await visita.findAll({
+      where: {
+        [Op.and]: [
+          { vs_data: { [Op.gte]: currentDate } },
+          { pt_id: id }]
+      },
+      order: [["vs_data", "ASC"]]
+    });
+    res.send({ success: true, data: data });
+  } catch (error) {
+    next(error)
+  }
 };
 
 controllers.createVisita = async (req, res, next) => {
-    const t = await sequelize.transaction();
-    try {
-        const visitas = await visita.create(
-            {
-                vs_data: req.body.data,
-                vs_horas: req.body.horas,
-                vs_vagas: req.body.vagas,
-                //pt_id: req.body.pt_id,
-                pt_id: 1,
-            },
-            { transaction: t }
-        );
-        await t.commit();
-        res.send({ success: true, data: visita });
-    } catch (e) {
-        await t.rollback();
-        next(e);
-    }
+  const t = await sequelize.transaction();
+  try {
+    const visitas = await visita.create(
+      {
+        vs_data: req.body.data,
+        vs_horas: req.body.horas,
+        vs_vagas: req.body.vagas,
+        //pt_id: req.body.pt_id,
+        pt_id: 1,
+      },
+      { transaction: t }
+    );
+    await t.commit();
+    res.send({ success: true, data: visita });
+  } catch (e) {
+    await t.rollback();
+    next(e);
+  }
 };
 
 controllers.getVisitaById = async (req, res, next) => {
-    try {
-        const {id} = req.params
-        const data = await visita.findByPk(id)
-        //check if id is not a number
-        res.send({ success: true, data: data });
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const { id } = req.params
+    const data = await visita.findByPk(id,
+      { include: [{ model: pontoTuristicos }] })
+    //check if id is not a number
+    res.send({ success: true, data: data });
+  } catch (error) {
+    next(error)
+  }
 }
 
 controllers.updateVisita = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        //check if id is not a number
-        if (isNaN(id)) return createError.BadRequest("id is not a number")
+  try {
+    const { id } = req.params;
+    //check if id is not a number
+    if (isNaN(id)) return createError.BadRequest("id is not a number")
 
-        const {dataVisita,hora,vagas} = req.body;
-        const data = await visita.update({
-            vs_data:dataVisita,
-            vs_horas:hora,
-            vs_vagas:vagas,
-        },
-            {
-                where: { vs_id: id }
-            })
-        res.send({ success: true, data: data, message: "Updated successful" });
-    } catch (error) {
-        next(error)
-    }
+    const { dataVisita, hora, vagas } = req.body;
+    const data = await visita.update({
+      vs_data: dataVisita,
+      vs_horas: hora,
+      vs_vagas: vagas,
+    },
+      {
+        where: { vs_id: id }
+      })
+    res.send({ success: true, data: data, message: "Updated successful" });
+  } catch (error) {
+    next(error)
+  }
 }
 
 controllers.deleteVisita = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        //check if id is not a number
-        if (isNaN(id)) return createError.BadRequest("id is not a number")
-        const del = await visita.destroy({
-            where: { vs_id: id }
-        })
-        res.send({ success: true, deleted: del, message: "Deleted successful" });
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const { id } = req.params;
+    //check if id is not a number
+    if (isNaN(id)) return createError.BadRequest("id is not a number")
+    const del = await visita.destroy({
+      where: { vs_id: id }
+    })
+    res.send({ success: true, deleted: del, message: "Deleted successful" });
+  } catch (error) {
+    next(error)
+  }
 }
 module.exports = controllers;
--- Backend/src/controllers/voucherController.js
+++ Backend/src/controllers/voucherController.js
@@ -1,5 +1,5 @@
 var voucher = require('../models/voucherModel');
-
+var cliente_voucher = require('../models/cliente_voucherModel')
 const controllers = {};
 var sequelize = require("../models/database");
 const Sequelize = require("sequelize");
@@ -7,79 +7,95 @@ const Op = Sequelize.Op;
 const createError = require('http-errors')
 
 controllers.getAllVoucher = async (req, res, next) => {
-    try {
-        const data = await voucher.findAll();
-        res.send({ success: true, data: data });
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const data = await voucher.findAll();
+    res.send({ success: true, data: data });
+  } catch (error) {
+    next(error)
+  }
 };
 
 controllers.createVoucher = async (req, res, next) => {
-    const t = await sequelize.transaction();
-    try {
-        const voucher = await voucher.create(
-            {
-                dtinicio: req.body.v_dtinicio,
-                dtfim: req.body.v_dtfim,
-                pontos: req.body.v_pontos,
-                emitidos: req.body.v_emitidos,
-            },
-            { transaction: t }
-        );
-        await t.commit();
-        res.send({ success: true, data: voucher });
-    } catch (e) {
-        await t.rollback();
-        next(e);
-    }
+  const t = await sequelize.transaction();
+  try {
+    const voucher = await voucher.create(
+      {
+        dtinicio: req.body.v_dtinicio,
+        dtfim: req.body.v_dtfim,
+        pontos: req.body.v_pontos,
+        emitidos: req.body.v_emitidos,
+      },
+      { transaction: t }
+    );
+    await t.commit();
+    res.send({ success: true, data: voucher });
+  } catch (e) {
+    await t.rollback();
+    next(e);
+  }
 };
 
 controllers.getVoucherById = async (req, res, next) => {
-    try {
-        const {id} = req.params
-        const data = await voucher.findByPk(id)
-        //check if id is not a number
-        res.send({ success: true, data: data });
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const { id } = req.params
+    const data = await voucher.findByPk(id)
+    //check if id is not a number
+    res.send({ success: true, data: data });
+  } catch (error) {
+    next(error)
+  }
+}
+
+controllers.getVoucherByUserId = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const data = await voucher.findAll(
+        { where: { c_id: id },
+            include:[{model:cliente_voucher, 
+                attributes:['c_disponibilidade']
+            }] 
+        }
+    )
+    res.send({ success: true, data: data });
+  } catch (error) {
+    next(error)
+  }
 }
 
 controllers.updateVoucher = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        //check if id is not a number
-        if (isNaN(id)) return createError.BadRequest("id is not a number")
+  try {
+    const { id } = req.params;
+    //check if id is not a number
+    if (isNaN(id)) return createError.BadRequest("id is not a number")
 
-        const {dtinicio,dtfim,pontos,emitidos} = req.body;
-        const data = await voucher.update({
-            v_dtinicio: dtinicio,
-            v_dtfim:dtfim,
-            v_pontos:pontos,
-            v_emitidos:emitidos,
+    const { dtinicio, dtfim, pontos, emitidos } = req.body;
+    const data = await voucher.update({
+      v_dtinicio: dtinicio,
+      v_dtfim: dtfim,
+      v_pontos: pontos,
+      v_emitidos: emitidos,
 
-        },
-            {
-                where: { v_id: id }
-            })
-        res.send({ success: true, data: data, message: "Updated successful" });
-    } catch (error) {
-        next(error)
-    }
+    },
+      {
+        where: { v_id: id }
+      })
+    res.send({ success: true, data: data, message: "Updated successful" });
+  } catch (error) {
+    next(error)
+  }
 }
 
 controllers.deleteVoucher = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        //check if id is not a number
-        if (isNaN(id)) return createError.BadRequest("id is not a number")
-        const del = await voucher.destroy({
-            where: { v_id: id }
-        })
-        res.send({ success: true, deleted: del, message: "Deleted successful" });
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const { id } = req.params;
+    //check if id is not a number
+    if (isNaN(id)) return createError.BadRequest("id is not a number")
+    const del = await voucher.destroy({
+      where: { v_id: id }
+    })
+    res.send({ success: true, deleted: del, message: "Deleted successful" });
+  } catch (error) {
+    next(error)
+  }
 }
 module.exports = controllers;
