6330376ebdf52a528d85ec11e5716b94115be61a
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2023-02-06 14:04:57 +0000

    remoção da caminhos de proteção

--- Backoffice/src/App.jsx
+++ Backoffice/src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import router from './routes/index'
-import RequireAuth from './components/RequireAuth'
-import PersistLogin from './components/PersistLogin'
-import Layout from './components/Layout'
 import Unauthorized from './components/Unauthorized'
 
-const ROLES = {
-  Admin: 1,
-  ResponsavelRT: 2,
-  AgenteT: 3,
-}
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
         <Route index element={<router.login />} />
         <Route
           path="/recuperarPalavraPasse"
@@ -27,7 +18,6 @@ export default function App() {
 
         {/*Persist Login*/}
         {/*Admin*/}
-        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
           <Route path="/dashboard" element={<router.dashboardAdmin />} />
           <Route path="/website" element={<router.websiteAdmin />} />
           <Route
@@ -39,10 +29,8 @@ export default function App() {
             element={<router.regioesturisticasAdmin />}
           />
           <Route path="/utilizadores" element={<router.utilizadoresAdmin />} />
-        </Route>
 
         {/*Responsavel da Região Turistica*/}
-        <Route element={<RequireAuth allowedRoles={[ROLES.ResponsavelRT]} />}>
           <Route
             path="/dashboardRRT"
             element={<router.dashboardRegiaoTuristica />}
@@ -56,18 +44,14 @@ export default function App() {
             element={<router.pontosDeInteresseRT />}
           />
           <Route path="/recompensas" element={<router.recompensasRT />} />
-        </Route>
 
         {/*Agente Turistico*/}
-        <Route element={<RequireAuth allowedRoles={[ROLES.AgenteT]} />}>
           <Route path="/dashboardAT" element={<router.dashboardAT />} />
           <Route path="criarvisita" element={<router.criarVisitaAT />} />
           <Route
             path="/consultarReservas"
             element={<router.consultarReservasAT />}
           />
-        </Route>
-      </Route>
     </Routes>
   )
 }
