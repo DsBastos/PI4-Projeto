d21058334758aa808a1e207245a73c49cbc90a29
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2023-09-05 17:29:54 +0100

    teste 2

--- Backoffice/vite.config.js
+++ Backoffice/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   root: path.resolve(__dirname, "src"),
   resolve: {
     alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
     },
   },
   server: {
